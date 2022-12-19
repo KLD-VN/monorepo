eab86d223c62ed3c4ec264b0f4db4c939f1358da
khanhld <khanhld@vnext.vn>
2022-12-19 18:20:12 +0700

    clean code

--- packages/shiba-talk-server/server.ts
+++ packages/shiba-talk-server/server.ts
@@ -1,9 +1,26 @@
-import express from 'express';
+import express from "express";
+import _ from "lodash";
 const app = express();
-const port = 3001;
+const port = 3003;
 
-app.get("/data", (req, res) => {
-  res.json({ foo: "bar" });
+export interface QueryPayload {
+  payload: string;
+}
+
+app.use((_req, res, next) => {
+  // Allow any website to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Continue to next middleware
+  next();
+});
+
+app.get("/", (_req, res) => {
+  const responseData: QueryPayload = {
+    payload: _.snakeCase("Server data returned successfully"),
+  };
+
+  res.json(responseData);
 });
 
 app.listen(port, () => {
